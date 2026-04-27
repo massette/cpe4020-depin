d8486b77d34e4bd58fc8498f3fef0d85fd4386a2
mamabeanie-bug <tyrellds19@gmail.com>
2026-04-27 15:09:08 -0400

    Update DEPIN_validator.py

--- DEPIN_validator.py
+++ DEPIN_validator.py
@@ -1,19 +1,18 @@
 import sys
 import time
+from datetime import datetime, timedelta
 import socket
 import select
 import secrets
 from threading import Thread, Event
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, request
 from cryptography.exceptions import InvalidSignature
-
+from ledger import add_block
 from lib.const import Time, Type, Address
+from lib.parse import Message
 from lib.bytes import concat
 
 app = Flask(__name__)
-
-# initialize validators
-validators = list(Address.VALIDATORS.keys())
 
 ################################################################ NODE DETAILS ##
 # parse arguments
@@ -22,10 +21,11 @@ if len(sys.argv) < 2:
     print()
 
     sys.exit(1)
-elif sys.argv[1] not in validators:
+elif sys.argv[1] not in Address.VALIDATORS:
     print(
         "Invalid ID {}, expected one of: {}".format(
-            sys.argv[1], ", ".join(validators)
+            sys.argv[1],
+            ", ".join(Address.VALIDATORS.keys())
         )
     )
     print()
@@ -36,16 +36,16 @@ NODE_ID = sys.argv[1]
 NODE_ADDR = Address.VALIDATORS[NODE_ID]
 
 ################################################################ POLL SOCKETS ##
-from listen import poll, send_all, update_transactions, wallets, keys, pending
+from listen import poll, send_all, keys, pending
 
 # run on separate thread
 sockets_thread = Thread(target=poll, args=(NODE_ADDR,))
 sockets_thread.start()
 
 ###################################################################### ROUTES ##
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.get("/")
+def get_dashboard():
+    return "dashboard :)", 200
 
 @app.post("/mint")
 def post_transaction():
@@ -73,7 +73,7 @@ def post_transaction():
     while (wallet_id, session_id) in pending:
         session_id = secrets.randbits(32)
 
-    # wait for response on other thread
+    #
     pending[wallet_id, session_id] = Event()
 
     # format signed mint request as TKN
@@ -90,72 +90,21 @@ def post_transaction():
     pending.pop((wallet_id, session_id))
 
     if is_done:
-        update_transactions()
-        return jsonify(transactions[-1]), 200
+        return "TKN", 200
     else:
         return "Request timed out.", 408
 
-@app.route("/wallets")
-def get_wallets():
-    return jsonify(wallets)
-
-@app.route("/wallets/<addr>")
-def get_wallet(addr):
-    if addr in wallets:
-        return wallets[addr], 200
-    else:
-        return "Wallet not found.", 404
-
-@app.route("/transactions")
+@app.get("/transactions")
 def get_transactions():
-    return jsonify(transactions)
+    return {"data": "<the whole ledger>"}, 200
 
-@app.route("/validators")
-def get_validators():
-    return jsonify(validators)
+@app.get("/wallets")
+def get_wallets():
+    return {"data": "<list of all wallets>"}, 200
 
-@app.post("/move")
-def post_move():
-    payload = request.data
-
-    # identify which wallet signed it
-    wallet_id = None
-
-    for w in Address.WALLETS:
-        try:
-            keys[w].unsign(payload)
-            wallet_id = w
-            break
-        except InvalidSignature:
-            continue
-
-    if wallet_id is None:
-        return "Could not verify signature.", 400
-
-    # create session
-    session_id = secrets.randbits(32)
-
-    while (wallet_id, session_id) in pending:
-        session_id = secrets.randbits(32)
-
-    pending[wallet_id, session_id] = Event()
-    # send EXACT payload to validators (just like mint)
-    mov = concat(
-        Type.MOV,
-        keys["validators"].encrypt(wallet_id, session_id, payload)
-    )
-
-    send_all(mov)
-
-    is_done = pending[wallet_id, session_id].wait(Time.TIMEOUT)
-    pending.pop((wallet_id, session_id))
-
-    if is_done:
-        update_transactions()
-        return jsonify(transactions[-1]), 200
-
-    return "Move request timed out.", 408
+@app.get("/wallets/<key>")
+def get_wallet(key):
+    return {"data": "<list of everything owned by the wallet>"}, 200
 
 ############################################################### LAUNCH SERVER ##
-if __name__ == "__main__":
-    app.run(host=Address.VALIDATOR_IP, port=6561)
+app.run(host="0.0.0.0", port=6561)
