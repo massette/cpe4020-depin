fe2cb528fc5b3371c5c9a94c9c767f7677e8a149
massette <featherboxen@gmail.com>
2026-04-27 12:52:14 -0400

    Update directory structure

--- DEPIN_validator.py
+++ DEPIN_validator.py
@@ -5,14 +5,41 @@ import socket
 import select
 import secrets
 from threading import Thread, Event
-from flask import Flask, request
+from flask import Flask, jsonify, render_template, request
 from cryptography.exceptions import InvalidSignature
 
 from lib.const import Time, Type, Address
+from lib.keys import Public
 from lib.parse import Message
 from lib.bytes import concat
 
+from ledger import load_ledger
+
 app = Flask(__name__)
+
+# initialize validators
+validators = list(Address.VALIDATORS.keys())
+
+# initialize wallets
+wallets = {}
+keys = {}
+
+for w in Address.WALLETS:
+    keys = Public("keys/{}.pub.pem".format(w))
+    addr = keys[w].reveal()
+    wallets[addr] = 0
+
+# load previous transactions
+def update_transactions():
+    global transactions = load_ledger()
+
+    for block in transactions:
+        if block["from"] != "MINT":
+            wallets[block["from"]] -= block["amount"]
+        
+        wallets[block["to"]] += block["amount"]
+
+update_transactions()
 
 ################################################################ NODE DETAILS ##
 # parse arguments
@@ -21,11 +48,10 @@ if len(sys.argv) < 2:
     print()
 
     sys.exit(1)
-elif sys.argv[1] not in Address.VALIDATORS:
+elif sys.argv[1] not in validators:
     print(
         "Invalid ID {}, expected one of: {}".format(
-            sys.argv[1],
-            ", ".join(Address.VALIDATORS.keys())
+            sys.argv[1], ", ".join(validators)
         )
     )
     print()
@@ -36,16 +62,16 @@ NODE_ID = sys.argv[1]
 NODE_ADDR = Address.VALIDATORS[NODE_ID]
 
 ################################################################ POLL SOCKETS ##
-from listen import poll, send_all, keys, pending
+from listen import poll, send_all, pending
 
 # run on separate thread
 sockets_thread = Thread(target=poll, args=(NODE_ADDR,))
 sockets_thread.start()
 
 ###################################################################### ROUTES ##
-@app.get("/")
-def get_dashboard():
-    return "dashboard :)", 200
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 @app.post("/mint")
 def post_transaction():
@@ -73,7 +99,7 @@ def post_transaction():
     while (wallet_id, session_id) in pending:
         session_id = secrets.randbits(32)
 
-    # 
+    # wait for response on other thread
     pending[wallet_id, session_id] = Event()
 
     # format signed mint request as TKN
@@ -90,21 +116,30 @@ def post_transaction():
     pending.pop((wallet_id, session_id))
 
     if is_done:
-        return "TKN", 200
+        update_transactions()
+        return jsonify(transactions[-1]), 200
     else:
         return "Request timed out.", 408
 
-@app.get("/transactions")
-def get_transactions():
-    return {"data": "<the whole ledger>"}, 200
-
-@app.get("/wallets")
+@app.route("/wallets")
 def get_wallets():
-    return {"data": "<list of all wallets>"}, 200
+    return jsonify(wallets)
 
-@app.get("/wallets/<key>")
-def get_wallet(key):
-    return {"data": "<list of everything owned by the wallet>"}, 200
+@app.route("/wallets/<addr>")
+def get_wallet(addr):
+    if addr in wallets:
+        return wallets[addr], 200
+    else:
+        return "Wallet not found.", 404
+
+@app.route("/transactions")
+def get_transactions():
+    return jsonify(transactions)
+
+@app.route("/validators")
+def get_validators():
+    return jsonify(validators)
 
 ############################################################### LAUNCH SERVER ##
-app.run(host=Address.HOST_IP, port=6561)
+if __name__ == "__main__":
+    app.run(debug=True, host=Address.VALIDATOR_IP, port=6561)
