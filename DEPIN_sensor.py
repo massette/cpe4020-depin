85bd9aaf244e7aafe732093ab705f3618d772226
mamabeanie-bug <tyrellds19@gmail.com>
2026-04-27 12:27:49 -0400

    Update DEPIN_sensor.py

--- DEPIN_sensor.py
+++ DEPIN_sensor.py
@@ -142,7 +142,7 @@ while True:
             "angle_change_deg": round(diff, 1),
             "prev_angle_deg": round(prev_angle, 1),
             "angle_deg": round(current_angle, 1),
-            "timestamp": datetime.utcnow().isoformat(),,
+            "timestamp": datetime.utcnow().isoformat(),
 #            },
 #            "timestamp": now,
 #            "pubkey_pem": wallet.pub_pem
