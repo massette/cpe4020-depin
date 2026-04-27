443cdd12d1c50717c86d115b4a83b9f885ae50d9
massette <featherboxen@gmail.com>
2026-04-27 12:51:23 -0400

    Integrate mint request code with REST API

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,22 +1,32 @@
-from flask import Flask, jsonify, render_template, request
+import sys
 import time
-import random
+from datetime import datetime, timedelta
+import socket
+import select
+import secrets
+from threading import Thread, Event
+from flask import Flask, jsonify, render_template, request
+from cryptography.exceptions import InvalidSignature
 
-from lib.const import Address
+from lib.const import Time, Type, Address
 from lib.keys import Public
+from lib.parse import Message
+from lib.bytes import concat
 
 from ledger import load_ledger
 
 app = Flask(__name__)
 
 # initialize validators
-validators = Address.VALIDATORS.keys()
+validators = list(Address.VALIDATORS.keys())
 
 # initialize wallets
 wallets = {}
+keys = {}
 
 for w in Address.WALLETS:
-    addr = Public("keys/{}.pub.pem".format(w)).reveal()
+    keys = Public("keys/{}.pub.pem".format(w))
+    addr = keys[w].reveal()
     wallets[addr] = 0
 
 # load previous transactions
@@ -29,7 +39,36 @@ def update_transactions():
         
         wallets[block["to"]] += block["amount"]
 
+update_transactions()
 
+################################################################ NODE DETAILS ##
+# parse arguments
+if len(sys.argv) < 2:
+    print("USAGE: python {} <VALIDATOR ID>".format(sys.argv[0]))
+    print()
+
+    sys.exit(1)
+elif sys.argv[1] not in validators:
+    print(
+        "Invalid ID {}, expected one of: {}".format(
+            sys.argv[1], ", ".join(validators)
+        )
+    )
+    print()
+
+    sys.exit(1)
+
+NODE_ID = sys.argv[1]
+NODE_ADDR = Address.VALIDATORS[NODE_ID]
+
+################################################################ POLL SOCKETS ##
+from listen import poll, send_all, pending
+
+# run on separate thread
+sockets_thread = Thread(target=poll, args=(NODE_ADDR,))
+sockets_thread.start()
+
+###################################################################### ROUTES ##
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -60,7 +99,7 @@ def post_transaction():
     while (wallet_id, session_id) in pending:
         session_id = secrets.randbits(32)
 
-    # 
+    # wait for response on other thread
     pending[wallet_id, session_id] = Event()
 
     # format signed mint request as TKN
@@ -86,6 +125,13 @@ def post_transaction():
 def get_wallets():
     return jsonify(wallets)
 
+@app.route("/wallets/<addr>")
+def get_wallet(addr):
+    if addr in wallets:
+        return wallets[addr], 200
+    else:
+        return "Wallet not found.", 404
+
 @app.route("/transactions")
 def get_transactions():
     return jsonify(transactions)
@@ -94,6 +140,6 @@ def get_transactions():
 def get_validators():
     return jsonify(validators)
 
-# -----------------------------
+############################################################### LAUNCH SERVER ##
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host=Address.VALIDATOR_IP, port=6561)
